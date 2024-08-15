d17e13bc8d5f8505670451b92d3370204f769189
JULI1926 <jdlondono8@misena.edu.co>
2024-08-15 16:32:15 -0500

    Se crea home page y se pone mensaje de bienvenida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,5 @@
-<div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-800">
-    <div class="px-4">
-        @auth
-            <div class="font-medium text-lg text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-            <div class="font-medium text-sm text-gray-500 dark:text-gray-400">{{ Auth::user()->email }}</div>
-        @else
-            <div class="font-medium text-lg text-gray-800 dark:text-gray-200">Guest</div>
-        @endauth
-    </div>
+@extends('layouts.template')
 
-    <div class="mt-3 space-y-1">
-        @auth
-            <x-responsive-nav-link :href="route('profile.edit')" class="block px-4 py-2 rounded-md text-base font-medium text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700">
-                {{ __('Profile') }}
-            </x-responsive-nav-link>
+@section('content')
 
-            <!-- Authentication -->
-            <form method="POST" action="{{ route('logout') }}" class="block px-4 py-2 rounded-md text-base font-medium text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700">
-                @csrf
-                <button type="submit">{{ __('Logout') }}</button>
-            </form>
-        @else
-            <x-responsive-nav-link :href="route('login')" class="block px-4 py-2 rounded-md text-base font-medium text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700">
-                {{ __('Login') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('register')" class="block px-4 py-2 rounded-md text-base font-medium text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700">
-                {{ __('Register') }}
-            </x-responsive-nav-link>
-        @endauth
-    </div>
-</div>
+@endsection
